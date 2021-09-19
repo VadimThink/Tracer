ec71fc536d46db1c6f6234b95443503fc71ca208
VadimThink <54996709+VadimThink@users.noreply.github.com>
2021-09-19 13:53:13 +0300

    Serializers set up

---
+++ TracerLibrary/Printer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace TracerLibrary
+{
+    public class Printer
+    {
+        private List<ISerializer> Serializers;
+        private List<StreamWriter> StreamWriters;
+
+        public Printer()
+        {
+            Serializers = new List<ISerializer>();
+            StreamWriters = new List<StreamWriter>();
+        }
+
+        public void AddStream(Stream stream)
+        {
+            StreamWriters.Add(new StreamWriter(stream));
+        }
+
+        public void AddStreamToFile(string fileName)
+        {
+            FileStream fileStream = File.Create(fileName);
+            StreamWriters.Add(new StreamWriter(fileStream));
+        }
+
+        public void AddSerializer(ISerializer serializer)
+        {
+            Serializers.Add(serializer);
+        }
+
+        public void Print(object data)
+        {
+            foreach (var serializer in Serializers)
+            {
+                foreach (var streamWriter in StreamWriters)
+                {
+                    serializer.Serialize(data, streamWriter);
+                }
+            }
+        }
+        
+    }
+}
---
+++ TracerLibrary/XmlSerializerAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace TracerLibrary
+{
+    public class XmlSerializerAdapter : ISerializer
+    {
+        public void Serialize(object o, StreamWriter streamWriter)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(o.GetType());
+            xmlSerializer.Serialize(streamWriter, o);
+            streamWriter.WriteLine("\n");
+            streamWriter.Flush();
+        }
+    }
+}
